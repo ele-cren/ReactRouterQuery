564aabbe28428fb6918e9f0cc0fbfe1bc79c819b
Erwan <lecren.erwan@gmail.com>
2019-10-07 19:18:07 +0200

    remove contexts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Appartements from './Containers/Appartements/Appartements'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { UserProvider } from './Contexts/UserContext'
 
 function App() {
   return (
-    <UserProvider value={ { name: "Erwan" } }>
-      <Router>
-        <Route exact path="/" component={ Appartements } />
-        <Route path="/appartement/:id" />
-      </Router>
-    </UserProvider>
+    <Router>
+      <Route exact path="/" component={ Appartements } />
+    </Router>
   )
 }
 
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,19 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import styles from './Item.module.css'
 import moment from 'moment'
-import { Link } from 'react-router-dom'
-import UserContext from '../../Contexts/UserContext'
 
 
 const Item = (props) => {
-    const { title, description, date, id } = props
-    const user = useContext(UserContext)
+    const { title, description, date } = props
     return (
         <div className={ [styles.itemContainer, !date ? styles.available : styles.notAvailable].join(' ') }>
-            <Link to={ "appartement/" + id }><h2>{ title }</h2></Link>
+            <h2>{ title }</h2>
             <p>{ description }</p>
             { date ? <p className={ styles.date } >{ moment.unix(date).format('DD/MM/YYYY') }</p> : '' }
-            <p>{ user.name }</p>
         </div>
     )
 }
